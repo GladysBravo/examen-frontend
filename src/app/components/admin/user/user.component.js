40b09e1ebbf9b40243833cfb9e7fd3cb1e577de8
Omar Gutierrez <ogutierrez@agetic.gob.bo>
2017-09-28 12:05:23 -0400

    Bootstrap 4 beta

--- src/app/components/admin/user/user.component.js
+++ src/app/components/admin/user/user.component.js
@@ -9,8 +9,8 @@ const UserComponent = {
         <div class="container">
             <h2 class="page-title"><i class="fa fa-user"></i> Usuarios <small><strong>del sistema</strong></small></h2>
             <div class="card">
-                <div class="card-block">
-                    <crud-table url="$ctrl.url" 
+                <div class="card-body">
+                    <crud-table url="$ctrl.url"
                                 ng-title="$ctrl.title"
                                 fields="$ctrl.fields"
                                 fields-create="$ctrl.fieldsCreate"></crud-table>
